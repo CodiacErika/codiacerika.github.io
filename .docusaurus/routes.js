44a8e97195bc94016fccaccc9673e68f989433ff
ebarcott <erika@codiac.io>
2024-05-24 09:16:33 -0700

    build with autogenerated sidebar

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -54,11 +54,11 @@ export default [
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs', '517'),
+    component: ComponentCreator('/docs', 'ba6'),
     routes: [
       {
         path: '/docs',
-        component: ComponentCreator('/docs', 'c2a'),
+        component: ComponentCreator('/docs', '29e'),
         routes: [
           {
             path: '/docs/tags',
@@ -77,195 +77,229 @@ export default [
           },
           {
             path: '/docs',
-            component: ComponentCreator('/docs', '0e0'),
+            component: ComponentCreator('/docs', '917'),
             routes: [
               {
                 path: '/docs/',
-                component: ComponentCreator('/docs/', '56e'),
+                component: ComponentCreator('/docs/', '4a8'),
                 exact: true,
-                sidebar: "docs"
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/concepts/advanced-concepts/blue-green-concepts',
-                component: ComponentCreator('/docs/concepts/advanced-concepts/blue-green-concepts', '0cf'),
-                exact: true
+                component: ComponentCreator('/docs/concepts/advanced-concepts/blue-green-concepts', 'c3e'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/concepts/advanced-concepts/cicd-concepts',
-                component: ComponentCreator('/docs/concepts/advanced-concepts/cicd-concepts', '66b'),
-                exact: true
+                component: ComponentCreator('/docs/concepts/advanced-concepts/cicd-concepts', 'aca'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/concepts/advanced-concepts/cluster-hopping-concepts',
-                component: ComponentCreator('/docs/concepts/advanced-concepts/cluster-hopping-concepts', 'e52'),
-                exact: true
+                component: ComponentCreator('/docs/concepts/advanced-concepts/cluster-hopping-concepts', '71b'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/concepts/advanced-concepts/rbac-concepts',
-                component: ComponentCreator('/docs/concepts/advanced-concepts/rbac-concepts', '643'),
-                exact: true
+                component: ComponentCreator('/docs/concepts/advanced-concepts/rbac-concepts', '8a3'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/concepts/basic-concepts/assets-concepts',
-                component: ComponentCreator('/docs/concepts/basic-concepts/assets-concepts', '31d'),
-                exact: true
+                component: ComponentCreator('/docs/concepts/basic-concepts/assets-concepts', '1c8'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/concepts/basic-concepts/cabinets-concepts',
-                component: ComponentCreator('/docs/concepts/basic-concepts/cabinets-concepts', '4b6'),
-                exact: true
+                component: ComponentCreator('/docs/concepts/basic-concepts/cabinets-concepts', '6c2'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/concepts/basic-concepts/clusters-concepts',
-                component: ComponentCreator('/docs/concepts/basic-concepts/clusters-concepts', '888'),
-                exact: true
+                component: ComponentCreator('/docs/concepts/basic-concepts/clusters-concepts', '143'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/concepts/basic-concepts/toolchain-reduction-concepts',
-                component: ComponentCreator('/docs/concepts/basic-concepts/toolchain-reduction-concepts', 'f74'),
-                exact: true
+                component: ComponentCreator('/docs/concepts/basic-concepts/toolchain-reduction-concepts', '8dc'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/concepts/what-is-codiac',
-                component: ComponentCreator('/docs/concepts/what-is-codiac', '8df'),
-                exact: true
+                component: ComponentCreator('/docs/concepts/what-is-codiac', 'c3d'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/get-started/cicd-integration',
-                component: ComponentCreator('/docs/get-started/cicd-integration', 'fb1'),
-                exact: true
+                component: ComponentCreator('/docs/get-started/cicd-integration', '9dd'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/get-started/create-account',
-                component: ComponentCreator('/docs/get-started/create-account', '027'),
-                exact: true
+                component: ComponentCreator('/docs/get-started/create-account', '96b'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/get-started/credentials',
-                component: ComponentCreator('/docs/get-started/credentials', 'ccc'),
-                exact: true
+                component: ComponentCreator('/docs/get-started/credentials', '3b7'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/get-started/install-cli',
-                component: ComponentCreator('/docs/get-started/install-cli', '6d7'),
-                exact: true
+                component: ComponentCreator('/docs/get-started/install-cli', '0d5'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/get-started/quickstart/deploy-10-microservices-quickstart',
-                component: ComponentCreator('/docs/get-started/quickstart/deploy-10-microservices-quickstart', '100'),
-                exact: true
+                component: ComponentCreator('/docs/get-started/quickstart/deploy-10-microservices-quickstart', '47a'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/get-started/quickstart/hello-world-quickstart',
-                component: ComponentCreator('/docs/get-started/quickstart/hello-world-quickstart', '16f'),
-                exact: true
+                component: ComponentCreator('/docs/get-started/quickstart/hello-world-quickstart', 'dc1'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/guides/cicd/existing-cicd-guide',
-                component: ComponentCreator('/docs/guides/cicd/existing-cicd-guide', '34d'),
-                exact: true
+                component: ComponentCreator('/docs/guides/cicd/existing-cicd-guide', 'c71'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/guides/cicd/github-guide',
-                component: ComponentCreator('/docs/guides/cicd/github-guide', 'fe9'),
-                exact: true
+                component: ComponentCreator('/docs/guides/cicd/github-guide', '11a'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/guides/cicd/gitlab-guide',
-                component: ComponentCreator('/docs/guides/cicd/gitlab-guide', '099'),
-                exact: true
+                component: ComponentCreator('/docs/guides/cicd/gitlab-guide', '1d5'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/guides/logging/datadog-guide',
-                component: ComponentCreator('/docs/guides/logging/datadog-guide', '217'),
-                exact: true
+                component: ComponentCreator('/docs/guides/logging/datadog-guide', '02e'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/guides/logging/elk-stack-guide',
-                component: ComponentCreator('/docs/guides/logging/elk-stack-guide', '575'),
-                exact: true
+                component: ComponentCreator('/docs/guides/logging/elk-stack-guide', '953'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/guides/logging/existing-logging-guide',
-                component: ComponentCreator('/docs/guides/logging/existing-logging-guide', '202'),
-                exact: true
+                component: ComponentCreator('/docs/guides/logging/existing-logging-guide', '348'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/guides/manage/cli-config-guide',
-                component: ComponentCreator('/docs/guides/manage/cli-config-guide', '486'),
-                exact: true
+                component: ComponentCreator('/docs/guides/manage/cli-config-guide', 'dc0'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/guides/manage/configuration-management-guide',
-                component: ComponentCreator('/docs/guides/manage/configuration-management-guide', '5e7'),
-                exact: true
+                component: ComponentCreator('/docs/guides/manage/configuration-management-guide', 'f94'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/guides/manage/move-deployments-guide',
-                component: ComponentCreator('/docs/guides/manage/move-deployments-guide', '82a'),
-                exact: true
+                component: ComponentCreator('/docs/guides/manage/move-deployments-guide', 'bff'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/guides/manage/recreate-cluster-guide',
-                component: ComponentCreator('/docs/guides/manage/recreate-cluster-guide', '534'),
-                exact: true
+                component: ComponentCreator('/docs/guides/manage/recreate-cluster-guide', 'b77'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/guides/manage/rotate-tokens-guide',
-                component: ComponentCreator('/docs/guides/manage/rotate-tokens-guide', 'e76'),
-                exact: true
+                component: ComponentCreator('/docs/guides/manage/rotate-tokens-guide', 'aef'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/guides/manage/upgrade-cluster-version-guide',
-                component: ComponentCreator('/docs/guides/manage/upgrade-cluster-version-guide', 'bac'),
-                exact: true
+                component: ComponentCreator('/docs/guides/manage/upgrade-cluster-version-guide', '6b4'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/guides/security/cli-security-guide',
-                component: ComponentCreator('/docs/guides/security/cli-security-guide', '56b'),
-                exact: true
+                component: ComponentCreator('/docs/guides/security/cli-security-guide', '898'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/guides/telemetry/existing-telemetry-guide',
-                component: ComponentCreator('/docs/guides/telemetry/existing-telemetry-guide', '478'),
-                exact: true
+                component: ComponentCreator('/docs/guides/telemetry/existing-telemetry-guide', 'e5f'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/guides/telemetry/prometheus-guide',
-                component: ComponentCreator('/docs/guides/telemetry/prometheus-guide', '505'),
-                exact: true
+                component: ComponentCreator('/docs/guides/telemetry/prometheus-guide', 'cdb'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/platform/autoscalers',
-                component: ComponentCreator('/docs/platform/autoscalers', '0e0'),
-                exact: true
+                component: ComponentCreator('/docs/platform/autoscalers', '0fc'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/platform/file-stores',
-                component: ComponentCreator('/docs/platform/file-stores', '9c5'),
-                exact: true
+                component: ComponentCreator('/docs/platform/file-stores', '8f7'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/platform/geoproximity',
-                component: ComponentCreator('/docs/platform/geoproximity', 'ccc'),
-                exact: true
+                component: ComponentCreator('/docs/platform/geoproximity', 'bb5'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/platform/probes',
-                component: ComponentCreator('/docs/platform/probes', '4d1'),
-                exact: true
+                component: ComponentCreator('/docs/platform/probes', '5fd'),
+                exact: true,
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/reference/cli-commands',
-                component: ComponentCreator('/docs/reference/cli-commands', 'c51'),
+                component: ComponentCreator('/docs/reference/cli-commands', 'cfe'),
                 exact: true,
-                sidebar: "docs"
+                sidebar: "tutorialSidebar"
               },
               {
                 path: '/docs/reference/glossary',
-                component: ComponentCreator('/docs/reference/glossary', 'fef'),
+                component: ComponentCreator('/docs/reference/glossary', '65f'),
                 exact: true,
-                sidebar: "docs"
+                sidebar: "tutorialSidebar"
               }
             ]
           }
